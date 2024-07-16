dc7a1d96fc78d0cff0ce0a3795dfb7c1cb445f0d
Manisha Singh <manishasinghbca89@gmail.com>
2024-07-17 02:12:43 +0530

    react component.

--- App.js
+++ App.js
@@ -5,7 +5,19 @@ import ReactDOM from "react-dom/client"
 // JSX is like html or xml syntax.
 // JSX => React.createElement => object => HtmlElement(render)
 //React Element
-    const jsxheading =( <h1 className="head" tabIndex="4">Manisha Here..........</h1>) ;
+
+    const jsxheading = ( 
+    <h1 className="head" tabIndex="4">
+        Manisha Here..........</h1>
+        );
+
+//React Functional Component
+    const CompHeading = () => (
+        <div> <h1 className="heading">First react component</h1></div>
+    );
+        
+    
+   // const CompHeading2 = () => <h1>First react component</h1>; //short way of creating arrow function ]in jsx.
     console.log(jsxheading );
      var root = ReactDOM.createRoot(document.getElementById("root"));
      root.render(jsxheading);
