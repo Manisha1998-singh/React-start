e79f8b99cbf9e84d4a42a501d922f37ab391f7c7
Manisha Singh <manishasinghbca89@gmail.com>
2024-07-17 01:43:09 +0530

    core react and jsx.

--- App.js
+++ App.js
@@ -1,55 +1,12 @@
-    // what if we want to create nested structure of html layout then how we create in react....
-    // Example.....
-    // <div>
-    //     <div>
-    //         <div>
-    //             <h1></h1>
-    //         </div>
-    //     </div>
-    // </div>
-    // var heading = React.createElement('div', {id: 'divfirst'},
-    //     React.createElement('div', {id: 'divsecond'}, React.createElement('div', {id: 'last'}, React.createElement('h1', {id: 'heading'}, "hii manisha.. this is my first react program"))));
-   
-    //below code is for creating siblings tags.
-    // Example.....
-    // <div>
-    //     <div>
-    //         <div>
-    //             <h1></h1>
-               //<h2></h2>
-    //         </div>
-    //     </div>
-    // </div>
-//var heading = React.createElement('div', {id: 'divfirst'},
-    //React.createElement('div', {id: 'divsecond'}, React.createElement('div', {id: 'last'}, [React.createElement('h1', {id: 'heading'}, "hii manisha.. this is my first react program"), React.createElement('h2', {id: 'heading1'}, "hii Anjali.. this is my first react program")] )));
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-    //below code is for creating two defferent section aur div in react.
-    // Example.....
-    // <div>
-    //     <div>
-    //         <div>
-    //             <h1></h1>
-               //<h2></h2>
-    //         </div>
-    //     </div>
-    // 
-    // <div>
-    //     <div>
-    //         <div>
-    //             <h1></h1>
-               //<h2></h2>
-    //         </div>
-    //     </div>
-    // </div>
-    //</div>
-    var heading = React.createElement('div', {id: 'divfirst'},
-        React.createElement('div', {id: 'divsecond'}, React.createElement('div', {id: 'last'}, [React.createElement('h1', {id: 'heading'}, "hii manisha.. this is my first react program"), React.createElement('h2', {id: 'heading1'}, "hii Anjali.. this is my first react program")] )),React.createElement('div', {id: 'divfirst'},
-            React.createElement('div', {id: 'divsecond'}, React.createElement('div', {id: 'last'}, [React.createElement('h1', {id: 'heading'}, "hii manisha.. this is my first react program"), React.createElement('h2', {id: 'heading1'}, "hii Anjali.. this is my first react program")] ))));
-        
-    
-console.log(heading);
+// React.createElement => object => HtmlElement(render)
+// JSX is like html or xml syntax.
+// JSX => React.createElement => object => HtmlElement(render)
+//React Element
+    const jsxheading =( <h1 className="head" tabIndex="4">Manisha Here..........</h1>) ;
+    console.log(jsxheading );
      var root = ReactDOM.createRoot(document.getElementById("root"));
-     root.render(heading);
-     //below code is for single div.
-     //var heading = React.createElement('h1', {id: 'heading'}, "hii manisha.. this is my first react program");
-     //below code is for creating siblings tags.
+     root.render(jsxheading);
+    
