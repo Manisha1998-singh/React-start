5b2303af790538b93009e2b403145bd2bb71d594
Manisha Singh <manishasinghbca89@gmail.com>
2024-07-17 23:51:40 +0530

    component composition.

--- App.js
+++ App.js
@@ -1,24 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
 // React.createElement => object => HtmlElement(render)
 // JSX is like html or xml syntax.
 // JSX => React.createElement => object => HtmlElement(render)
 //React Element
 
-    const jsxheading = ( 
-    <h1 className="head" tabIndex="4">
-        Manisha Here..........</h1>
-        );
+const jsxheading = (
+  <h1 className="head" tabIndex="4">
+    Manisha Here..........
+  </h1>
+);
+
+//React another  component
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX.
+  </h1>
+);
 
 //React Functional Component
-    const CompHeading = () => (
-        <div> <h1 className="heading">First react component</h1></div>
-    );
-        
-    
-   // const CompHeading2 = () => <h1>First react component</h1>; //short way of creating arrow function ]in jsx.
-    console.log(jsxheading );
-     var root = ReactDOM.createRoot(document.getElementById("root"));
-     root.render(jsxheading);
-    
+//Here we learn how to render component into the another component.
+//Here Title is the component
+const CompHeading = () => (
+  <div>
+    <Title />
+    <h1 className="heading">First react component</h1>
+  </div>
+);
+
+// const CompHeading2 = () => <h1>First react component</h1>; //short way of creating arrow function ]in jsx.
+//Component Render
+console.log(jsxheading);
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<CompHeading />);
